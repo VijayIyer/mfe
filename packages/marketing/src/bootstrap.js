68938f283ba2c948a6662b4020c089fc8c5dd822
vijay <VijayIyer>
2024-09-08 15:31:17 -0400

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// Mount function to startup the app
+const mount = (el) => {
+    ReactDOM.render(
+        <App />,
+        el)
+}
+// if we are in development and in isolation, call mount immediately
+if(process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector("#_marketing-dev-root");
+    if(devRoot) {
+        mount(devRoot);
+    }
+}
+// else export mount function
+export { mount };
