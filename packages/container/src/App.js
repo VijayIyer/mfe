70e4b588f1adf402742703d446b4799d860a521e
vijay <VijayIyer>
2024-09-20 23:34:34 -0400

    Temporarily comment out StylesProvider

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import {
-  StylesProvider,
-  createGenerateClassName,
-} from '@material-ui/core/styles';
-
-import MarketingApp from './components/MarketingApp';
-import Header from './components/Header';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import {StylesProvider, createGenerateClassName} from "@material-ui/core/styles";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'co',
-});
+    productionPrefix: 'co'
+})
 
 export default () => {
-  return (
-    <BrowserRouter>
-      <StylesProvider generateClassName={generateClassName}>
-        <div>
-          <Header />
-          <MarketingApp />
-        </div>
-      </StylesProvider>
-    </BrowserRouter>
-  );
-};
+    return (
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
+        </BrowserRouter>
+    )
+}
