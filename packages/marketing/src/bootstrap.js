ff4ef81b20aad795e1f2dfd2d16d4cdf096cf43f
vijay <VijayIyer>
2024-09-21 03:17:41 -0400

    Pass history object to memory router in sub-app

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory } from 'history';
 import App from './App';
 // Mount function to startup the app
 const mount = (el) => {
+    const history = createMemoryHistory();
     ReactDOM.render(
-        <App />,
+        <App hitory={history} />,
         el)
 }
 // if we are in development and in isolation, call mount immediately
